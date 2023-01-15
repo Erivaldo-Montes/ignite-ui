5f876b2eddc8ad174e515003cd5bca053907f080
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-01-15 11:18:52 -0300

    box docs

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,23 @@
+import { Box, BoxProps, Text } from '@ignite-ui/react/dist/index'
+import type { StoryObj, Meta } from '@storybook/react'
+
+export default {
+  title: 'FORM/box',
+  component: Box,
+  args: {
+    children: (
+      <>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+          provident nam ipsum fuga cum eaque, incidunt distinctio neque dolor
+          aliquid eos, voluptates temporibus tempora porro, repellat aliquam
+          maxime ipsa voluptatibus!
+        </Text>
+      </>
+    ),
+  },
+} as Meta<BoxProps>
+
+// variações do components (obrigatórios)
+
+export const Primary: StoryObj<BoxProps> = {}
