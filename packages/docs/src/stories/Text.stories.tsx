044a7c8bf2fa357b88d3c5079eb96ecb659f2e1f
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-01-15 11:19:22 -0300

    text docs

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,21 @@
+import { Text, TextProps } from '@ignite-ui/react/dist/index'
+import type { StoryObj, Meta } from '@storybook/react'
+
+export default {
+  title: 'typography/text',
+  component: Text,
+  args: {
+    children: 'Example text',
+  },
+} as Meta<TextProps>
+
+// variações do components (obrigatórios)
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    size: 'lg',
+    as: 'strong',
+  },
+}
