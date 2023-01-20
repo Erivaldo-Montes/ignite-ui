9c7e4366364950c18022d5fdb9df4e61e778a479
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-01-20 15:27:40 -0300

    mult step stories

---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,33 @@
+import { MultiStep, MultiStepProps, Box } from '@ignite-ui/react/dist/'
+import type { StoryObj, Meta } from '@storybook/react'
+
+export default {
+  title: 'form/multi step',
+  component: MultiStep,
+  args: {
+    size: 4,
+    currentStep: 1,
+  },
+  decorators: [
+    (story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+        >
+          {story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<MultiStepProps>
+
+// variações do components (obrigatórios)
+
+export const Primary: StoryObj<MultiStepProps> = {}
+
+export const Full: StoryObj<MultiStepProps> = {
+  args: {
+    currentStep: 4,
+  },
+}
