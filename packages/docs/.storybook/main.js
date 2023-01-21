c34a9fdd6f6c8b1fe8f09861d9505e25a0a44fe2
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-01-21 12:35:34 -0300

    add accessibility addon

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -6,7 +6,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y"
   ],
   "framework": "@storybook/react",
   "core": {
@@ -14,5 +15,7 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
-  }
+  },
+
+  
 }
